d59706eabedda22e6ec9a503db31ea2e59f4b9c6
CarolineMwangi <72011447+CarolineMwangi@users.noreply.github.com>
2020-11-15 12:22:21 +0300

    Order Status Page

---
+++ status.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>ORDER STATUS</title>
+
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+    </script>
+
+    <script type="text/javascript" src="status.js"></script>
+  
+</head>
+<body>
+
+<div>
+    <br>
+    <h1>ORDER STATUS</h1>
+
+
+    <form action="" method="post">
+        
+         <p>Order ID:</p>  
+         <input type="text" name="ID" placeholder="Enter Order ID" id="status-id" required>
+         <br>
+         <br>
+         <button id="st-btn">CHECK STATUS</button>
+    
+    </form>
+    </div>
+
+   
+
+</body>
+</html>
